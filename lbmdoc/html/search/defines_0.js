23b86a7a03ebab6c5f13ffd7bb4ab79afbf0e4f0
Joel Svensson <svenssonjoel@yahoo.se>
2022-02-11 13:07:35 +0100

    update the lispbm documentation

--- lbmdoc/html/search/defines_0.js
+++ lbmdoc/html/search/defines_0.js
@@ -1,4 +1,7 @@
 var searchData=
 [
+  ['lbm_5fmajor_5fversion',['LBM_MAJOR_VERSION',['../lbm__version_8h.html#a6785a47f4197583d00f0ed36a6d2d939',1,'lbm_version.h']]],
+  ['lbm_5fminor_5fversion',['LBM_MINOR_VERSION',['../lbm__version_8h.html#a034f184fa6a4f001f604bec5cb33e10c',1,'lbm_version.h']]],
+  ['lbm_5fpatch_5fversion',['LBM_PATCH_VERSION',['../lbm__version_8h.html#ab712a03219f3212f7635b6ffda5b6398',1,'lbm_version.h']]],
   ['lbm_5fval_5ftype_5fchar',['LBM_VAL_TYPE_CHAR',['../heap_8h.html#ad3eb1f0a9f3330d99205a71d58e365d8',1,'heap.h']]]
 ];
--- lbmdoc/html/search/files_4.js
+++ lbmdoc/html/search/files_4.js
@@ -3,5 +3,6 @@ var searchData=
   ['lbm_5fc_5finterop_2eh',['lbm_c_interop.h',['../lbm__c__interop_8h.html',1,'']]],
   ['lbm_5fmemory_2eh',['lbm_memory.h',['../lbm__memory_8h.html',1,'']]],
   ['lbm_5ftypes_2eh',['lbm_types.h',['../lbm__types_8h.html',1,'']]],
+  ['lbm_5fversion_2eh',['lbm_version.h',['../lbm__version_8h.html',1,'']]],
   ['lispbm_2eh',['lispbm.h',['../lispbm_8h.html',1,'']]]
 ];
